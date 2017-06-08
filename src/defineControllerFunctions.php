fb2b4a6aeb63eb33ea8a7bce3ef55a14f06a771a
Diego Bentes <diegopbentes@gmail.com>
2017-06-08 15:48:44 -0300

    Add support to object  in views

--- src/defineControllerFunctions.php
+++ src/defineControllerFunctions.php
@@ -6,7 +6,6 @@ use \UPhp\ActionController\ActionController;
 //if (! function_exists('view')) {
     function view($viewObject, $options = [])
     {
-        //var_dump($this);
         $classController = "controllers\\" . ucwords($viewObject->controllerName) . "Controller";
         $actionController = new $classController();
         $actionController->render($viewObject, $options);
